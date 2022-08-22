bfc0802eee49955de5d86dd04d7b79b8a8634038
Vladimir Burlakov <vvb@fgts.ru>
2022-08-22 15:54:42 +0300

    add create tables args and methods

--- main.go
+++ main.go
@@ -16,12 +16,29 @@ const PW_TABLE_NAME  = "pw_passwords"
 
 func main() {
     // Collect arguments 
-    var cli_pw  string
-    var db_name string
-    flag.StringVar(&cli_pw, "key", "", "key to decrypt passwords")
-    flag.StringVar(&db_name, "dbname", "store.db", "name for sqlite3 db file")
+    var cli_pw        string
+    var db_name       string
+    var creat_tables  bool
+    flag.StringVar(&cli_pw, "key","",         "key to decrypt passwords")
+    flag.StringVar(&db_name,"db", "store.db", "name for sqlite3 db file")
+    flag.BoolVar(&ls_res,   "ls", false,      "list available resources")
+    flag.BoolVar(&creat_tables, "create-tables", false,      
+                 "create tables (you need to drop it if exists)")
     flag.Parse()
 
+
+    // Open DB conn
+    db, err := sql.Open("sqlite3", db_name)
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
+
+    // Create tables can be done without password, so let's handle it first
+    if creat_tables {
+        check_tables(db_name, db)
+        return 
+    }
     
     // Check for decrypt password 
     var PW string
@@ -37,18 +54,6 @@ func main() {
         fmt.Println("No decrypt password was set, consider to use -key key or set PWMAN_PW var")
         os.Exit(1)
     }
-
-
-
-    // Open DB conn
-    db, err := sql.Open("sqlite3", db_name)
-    if err != nil {
-        panic(err)
-    }
-    defer db.Close()
-
-    init_tables(db)
-
 }
 
 
@@ -96,15 +101,19 @@ func create_default_table(table string, db *sql.DB) {
 }
 
 
-func init_tables(db *sql.DB) {
+func check_tables(db_name string, db *sql.DB) {
     // Create default tables if not exists
     if !is_table_exists(RES_TABLE_NAME, db) {
-        fmt.Println("table <" + RES_TABLE_NAME + "> does not exists, creating it..")
+        fmt.Println("table <" + RES_TABLE_NAME + "> does not exists in [" + db_name + "], creating it..")
         create_default_table(RES_TABLE_NAME, db)
+    } else {
+        fmt.Println("table <" + RES_TABLE_NAME + "> already exists in [" + db_name + "], do nothing.")
     }
 
     if !is_table_exists(PW_TABLE_NAME, db) {
-        fmt.Println("table <" + PW_TABLE_NAME + "> does not exists, creating it..")
+        fmt.Println("table <" + PW_TABLE_NAME + "> does not exists in [" + db_name + "], creating it..")
         create_default_table(PW_TABLE_NAME, db)
+    } else {
+        fmt.Println("table <" + PW_TABLE_NAME + "> already exists in [" + db_name + "], do nothing.")
     }
 }
