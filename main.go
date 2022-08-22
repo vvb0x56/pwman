82f4ee2a7121660ce54d4280278563b9d303a299
Vladimir Burlakov <vvb@fgts.ru>
2022-08-22 15:32:48 +0300

    add arguments parsing by flag

--- main.go
+++ main.go
@@ -5,41 +5,49 @@ import (
     "database/sql"
     "fmt"
     "os"
-    "log"
+    "flag"
 
     _ "github.com/mattn/go-sqlite3"
 )
-
 
 const RES_TABLE_NAME = "pw_resources"
 const PW_TABLE_NAME  = "pw_passwords"
 
 
 func main() {
-    create_tables()
-    // Check for password in env
-    var PW = os.Getenv("PWMAN_PW")
+    // Collect arguments 
+    var cli_pw  string
+    var db_name string
+    flag.StringVar(&cli_pw, "key", "", "key to decrypt passwords")
+    flag.StringVar(&db_name, "dbname", "store.db", "name for sqlite3 db file")
+    flag.Parse()
+
+    
+    // Check for decrypt password 
+    var PW string
+    if len(cli_pw) > 0 {
+        PW = cli_pw
+    } else {
+       var env_pw = os.Getenv("PWMAN_PW")
+       if len(env_pw) > 0 {
+           PW = env_pw
+       }
+    }
     if len(PW) == 0 {
-        log.Fatalln("No PWMAN_PW env variable was set.") 
+        fmt.Println("No decrypt password was set, consider to use -key key or set PWMAN_PW var")
+        os.Exit(1)
     }
 
-    db, err := sql.Open("sqlite3", "store.db")
+
+
+    // Open DB conn
+    db, err := sql.Open("sqlite3", db_name)
     if err != nil {
         panic(err)
     }
     defer db.Close()
 
-    if !is_table_exists(RES_TABLE_NAME, db) {
-        fmt.Println("table <" + RES_TABLE_NAME + "> does not exists")
-        return 
-    }
-
-    if !is_table_exists(PW_TABLE_NAME, db) {
-        fmt.Println("table <" + PW_TABLE_NAME + "> does not exists")
-        return 
-    }
-
-
+    init_tables(db)
 
 }
 
@@ -59,26 +67,44 @@ func is_table_exists(tablename string, db *sql.DB) bool {
 }
 
 
-//func create_tables(db *sql.DB) {
-func create_tables() {
-    res_query := "create table " + RES_TABLE_NAME + "(id INTEGER PRIMARY KEY, resource TEXT)"
-    pw_query  := "create table " + PW_TABLE_NAME + `(
-        id INTEGER PRIMARY KEY, 
-         resource_id INTEGER, 
-         username TEXT, 
-         password TEXT, 
-         type TEXT, 
-         FOREIGN KEY(resource_id) REFERENCES pw_resources(id)
-    )`
+func create_default_table(table string, db *sql.DB) {
+    var query string
+    switch {
+    case table == RES_TABLE_NAME:
+        query = "CREATE TABLE IF NOT EXISTS " + RES_TABLE_NAME + `(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            resource TEXT UNIQUE
+        )`
+    case table == PW_TABLE_NAME:
+        query = "CREATE TABLE IF NOT EXISTS " + PW_TABLE_NAME + `(
+             id INTEGER PRIMARY KEY AUTOINCREMENT, 
+             resource_id INTEGER, 
+             username TEXT, 
+             password TEXT, 
+             type TEXT, 
+             FOREIGN KEY(resource_id) REFERENCES pw_resources(id)
+        )`
+    default:
+        fmt.Println("Unknown table name in create_default_table: " + table)
+        return
+    }
 
-    fmt.Println(res_query)
-    fmt.Println(pw_query)
-
-    return 
+    _, err := db.Exec(query)
+    if err != nil {
+        panic(err)
+    }
 }
 
 
-//func create_table(query string, db *sql.DB) {
-//    return 
-//}
+func init_tables(db *sql.DB) {
+    // Create default tables if not exists
+    if !is_table_exists(RES_TABLE_NAME, db) {
+        fmt.Println("table <" + RES_TABLE_NAME + "> does not exists, creating it..")
+        create_default_table(RES_TABLE_NAME, db)
+    }
 
+    if !is_table_exists(PW_TABLE_NAME, db) {
+        fmt.Println("table <" + PW_TABLE_NAME + "> does not exists, creating it..")
+        create_default_table(PW_TABLE_NAME, db)
+    }
+}
